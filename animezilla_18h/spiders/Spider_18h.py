d9d115f77a3f4bea6c6b33ecc39a77c660b093ef
mario0729 <1142778722@qq.com>
2018-08-25 20:00:14 +0800

    修复
    修复最后一页下载

--- animezilla_18h/spiders/Spider_18h.py
+++ animezilla_18h/spiders/Spider_18h.py
@@ -6,23 +6,19 @@ from animezilla_18h.items import Animezilla18HItem
 class Spider18hSpider(scrapy.Spider):
     name = 'Spider_18h'
     allowed_domains = ['18h.animezilla.com']
-    start_urls = ['https://18h.animezilla.com/manga/3429']
-    for i in range(1,1000):
+    start_urls = []
+    #for url in open(r'D:\Spider_download\animezilla_18h_images\1.txt'): 
+        #url = "'"+url.strip()+"'"
+        #print(url) 
+        #start_urls.append(url)
+    for i in range(80,100):
         url = 'https://18h.animezilla.com/manga/'+str(i)
-        start_urls.append(url)
+        r = requests.get(url)
+        if r.status_code == 200:
+            print('%s yes!!!'%url)
+            start_urls.append(url)
 
     def parse(self, response):
-        if response.status == 200:
-            url = response.url
-            yield scrapy.Request(url=url,callback=self.parse_url)    
-            f = open(r'D:\Spider_download\animezilla_18h_images\1.txt', 'a+')
-            f.write(url)
-            f.write('\n')
-            f.close()
-        else:
-            print('%s不存在!!!!'%url)
-
-    def parse_url(self, response):
         #comicname表示漫画名字
         #page 表示当前页数
         #page_num表示漫画总页数
@@ -39,13 +35,13 @@ class Spider18hSpider(scrapy.Spider):
 
         i = 1
         while i <= int(page_num):
-            url = 'https://18h.animezilla.com/manga/3429/'+str(i)
+            url = response.url+'/'+str(i)
             yield scrapy.Request(url = url,callback=self.parse_image,meta={'comicname':comicname,'page':i})
             i+=1
 
     def parse_image(self, response):
         item = Animezilla18HItem()
-        item['image_urls'] = [response.css('#page-current img::attr(src)').extract_first()]
+        item['image_urls'] = [response.css('.entry-content p img::attr(src)').extract_first()]
         item['image_name'] = response.meta['comicname']
         item['page'] = response.meta['page']
         item['url'] = response.url
